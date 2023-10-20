e0282d9b3db49ed22969e534f82e766c37b35558
cheque17 <ezequielperezrojas1999@users.noreply.github.com>
2023-10-19 21:39:48 -0600

    Edit dist/index.html src/index.js and src/style.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import "./style.css";
 
+document.querySelector(".trigger").addEventListener("click", () => {
+  document.querySelector(".dropdown").classList.toggle("active");
+});
 
-document.querySelector('.trigger').addEventListener('click', () => {
-  document.querySelector('.dropdown').classList.toggle('active');
+function changeImage (imagesNode, currentImage, newIndexNumber) {
+  const target = imagesNode;
+  target[currentImage].classList.toggle('showing');
+  target[newIndexNumber].classList.toggle('showing')
+}
+
+
+const images = document.querySelectorAll(".image-display");
+
+let currentImage = 0;
+
+document.querySelector("#previous").addEventListener("click", () => {
+  if (currentImage === 0) {
+    const nextImgIndex = 3;
+    changeImage(images, currentImage, nextImgIndex);
+    currentImage = nextImgIndex;
+  } else {
+    const nextImgIndex = currentImage - 1;
+    changeImage(images, currentImage, nextImgIndex);
+    currentImage = nextImgIndex;
+  }
+});
+
+document.querySelector('#next').addEventListener('click', () => {
+  if (currentImage === 3){
+    const newIndexNumber = 0;
+    changeImage(images, currentImage, newIndexNumber);
+    currentImage = newIndexNumber;
+  } else {
+    const newIndexNumber = currentImage + 1;
+    changeImage(images, currentImage, newIndexNumber);
+    currentImage = newIndexNumber;
+  }
 })
+
+console.log(images);
